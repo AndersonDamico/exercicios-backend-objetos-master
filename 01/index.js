83df4d3b513897d7ca571ccd8b5605c710afb662
AndersonDamico <andersondamico2@gmail.com>
2023-07-04 13:38:04 -0300

    exercicio 01

---
+++ 01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua:'Mateus',
+    numero:'26',
+    complemento: 'parada',
+    bairro: 'jardim',
+    cep: '09002-003',
+    cidade: 'São Paulo',
+    estado: 'São Paulo',
+    pais: 'Brasil'
+};
+
+const usuario = {
+    nome:'Anderson',
+    email:'Anderson@gmail.com',
+    telefone:'11-39419228',
+    data_nascimento:'29/12/1994',
+    endereco
+};
+
+console.log(usuario)
